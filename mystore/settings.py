df9f101ead38b6ddac3bd41e8c5a9fbbe85294b1
ACHIWU\hi101072645 <hi101072645@gmail.com>
2017-08-20 21:06:27 +0800

    實作圖片上傳功能

--- mystore/settings.py
+++ mystore/settings.py
@@ -127,3 +127,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
